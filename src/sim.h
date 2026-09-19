6261d9bd8f15e9715ac73ea577715217feee47d5
Segen Stoutamire <segen@stoutamire.com>
2026-09-18 20:54:28 -0700

    misc(all): Add COPYING and license notices to tops of files

--- src/sim.h
+++ src/sim.h
@@ -1,3 +1,19 @@
+/* sim.h - data structures for an n-body simulation.
+   Copyright (C) 2026 Segen Stoutamire
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <https://www.gnu.org/licenses/>. */
+
 #ifndef NSOLAR_SIM_H_
 #define NSOLAR_SIM_H_
 
